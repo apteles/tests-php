51e15f0404df01ab58270abd7ec3df0c55fd8cbb
Andre Teles <andre.telestp@gmail.com>
2020-09-10 12:24:21 -0300

    chore(mock): add mockery and added test by example

--- tests/Models/OrderTest.php
+++ tests/Models/OrderTest.php
@@ -13,12 +13,29 @@ class FooPayment implements PaymentGatewayInterface
 }
 class OrderTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function test_order_should_be_able_to_process()
     {
         /** @var PaymentGatewayInterface&\PHPUnit\Framework\MockObject\MockObject $gwMock */
         $gwMock = $this->getMockBuilder(FooPayment::class)->onlyMethods(['charge'])->getMock();
 
         $gwMock->expects($this->once())->method('charge')->with($this->equalTo(200))->willReturn(true);
+        $order = new Order($gwMock);
+        $order->amount = 200;
+
+        $this->assertTrue($order->process());
+    }
+
+    public function test_order_should_be_able_to_process_with_mockery()
+    {
+        /** @var PaymentGatewayInterface&\Mockery\MockInterface&\Mockery\LegacyMockInterface $gwMock */
+        $gwMock = Mockery::mock(FooPayment::class);
+        $gwMock->shouldReceive('charge')->once()->with(200)->andReturn(true);
+
         $order = new Order($gwMock);
         $order->amount = 200;
 
