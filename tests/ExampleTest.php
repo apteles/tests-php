61092cd4f209dfbf5db329d2a824804538ad6805
Andre Teles <andre.telestp@gmail.com>
2020-09-10 09:11:33 -0300

    chore: initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_it_should_assert_true()
+    {
+        $this->assertTrue(true);
+    }
+}
